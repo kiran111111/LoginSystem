ac3b622c7fcc57ba65eec1de25307e4619dc2507
kiran <kiran111candy@gmail.com>
2020-04-08 19:18:01 -0400

    go

--- config/auth.js
+++ config/auth.js
@@ -4,7 +4,8 @@ const mongo = require("mongo");
 const mongoose = require("mongoose");
 
 
-const db = process.env.MONGODB_URI;
+// const db = process.env.MONGODB_URI ;
+const db ="mongodb+srv://kiran:kiran@cluster0-zrsby.mongodb.net/test?retryWrites=true&w=majority";
 
 const ConnectDB = async ()=>{
   try{
